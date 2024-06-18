998881aff87497749d929756d38838d39dce4fbc
yoomster <nperenboom@hotmail.com>
2024-06-18 23:18:58 +0200

    Add project files.

---
+++ VanDerBrinckesBakfietsenVerdieping/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanDerBrinckesBakfietsenVerdieping.Models
+{
+    public class EmployeeModel
+    {
+        public string LoginName { get; set; }
+        public string LoginPassword { get; set; }
+        public string FirstName{ get; set; }
+    }
+}
---
+++ VanDerBrinckesBakfietsenVerdieping/Models/RentalModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanDerBrinckesBakfietsenVerdieping.Models
+{
+    public class RentalModel
+    {
+        public double TotalCostBike { get; ; }
+        public double DailyCostExtras { get; set; }
+        public double TotalCostExtras{ get; set; }
+        public int NumberOfRentDays { get; set; }
+    }
+}
