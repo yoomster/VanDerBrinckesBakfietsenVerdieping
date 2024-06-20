1daafc54f2b41d4f5cd4b4be4c12c3be4d347dc6
yoomster <nperenboom@hotmail.com>
2024-06-20 21:39:11 +0200

    Add rental form designed

--- VanDerBrinckesBakfietsenVerdieping/FormAddRental.Designer.cs
+++ VanDerBrinckesBakfietsenVerdieping/FormAddRental.Designer.cs
@@ -28,12 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormAddRental";
+            CostsPerDayLabel = new Label();
+            totalDaysLabel = new Label();
+            ReturnDatePicker = new DateTimePicker();
+            label1 = new Label();
+            StartLabel = new Label();
+            RentDateLlabel = new Label();
+            StartDatePicker = new DateTimePicker();
+            ChoiceOfBikes = new ComboBox();
+            ExtrasList = new CheckedListBox();
+            ButtonCalculateCost = new Button();
+            TotalCostLabel = new Label();
+            ExtrasLabel = new Label();
+            BikeLabel = new Label();
+            SuspendLayout();
+            // 
+            // CostsPerDayLabel
+            // 
+            CostsPerDayLabel.AutoSize = true;
+            CostsPerDayLabel.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            CostsPerDayLabel.Location = new Point(114, 274);
+            CostsPerDayLabel.Name = "CostsPerDayLabel";
+            CostsPerDayLabel.Size = new Size(177, 21);
+            CostsPerDayLabel.TabIndex = 25;
+            CostsPerDayLabel.Text = "Kosten per dag zijn € 20";
+            // 
+            // totalDaysLabel
+            // 
+            totalDaysLabel.AutoSize = true;
+            totalDaysLabel.Font = new Font("Segoe UI", 12F);
+            totalDaysLabel.Location = new Point(433, 213);
+            totalDaysLabel.Name = "totalDaysLabel";
+            totalDaysLabel.Size = new Size(147, 21);
+            totalDaysLabel.TabIndex = 24;
+            totalDaysLabel.Text = "Totaal aantal dagen ";
+            // 
+            // ReturnDatePicker
+            // 
+            ReturnDatePicker.Font = new Font("Segoe UI", 12F);
+            ReturnDatePicker.Location = new Point(433, 157);
+            ReturnDatePicker.Name = "ReturnDatePicker";
+            ReturnDatePicker.Size = new Size(253, 29);
+            ReturnDatePicker.TabIndex = 23;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F);
+            label1.Location = new Point(452, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 21);
+            label1.TabIndex = 22;
+            label1.Text = "Eind";
+            // 
+            // StartLabel
+            // 
+            StartLabel.AutoSize = true;
+            StartLabel.Font = new Font("Segoe UI", 12F);
+            StartLabel.Location = new Point(450, 54);
+            StartLabel.Name = "StartLabel";
+            StartLabel.Size = new Size(42, 21);
+            StartLabel.TabIndex = 21;
+            StartLabel.Text = "Start";
+            // 
+            // RentDateLlabel
+            // 
+            RentDateLlabel.AutoSize = true;
+            RentDateLlabel.Font = new Font("Segoe UI", 12F);
+            RentDateLlabel.Location = new Point(433, 26);
+            RentDateLlabel.Name = "RentDateLlabel";
+            RentDateLlabel.Size = new Size(94, 21);
+            RentDateLlabel.TabIndex = 20;
+            RentDateLlabel.Text = "Huur datum";
+            // 
+            // StartDatePicker
+            // 
+            StartDatePicker.Font = new Font("Segoe UI", 12F);
+            StartDatePicker.Location = new Point(433, 83);
+            StartDatePicker.MinDate = new DateTime(2024, 3, 28, 0, 0, 0, 0);
+            StartDatePicker.Name = "StartDatePicker";
+            StartDatePicker.Size = new Size(253, 29);
+            StartDatePicker.TabIndex = 19;
+            // 
+            // ChoiceOfBikes
+            // 
+            ChoiceOfBikes.Font = new Font("Segoe UI", 12F);
+            ChoiceOfBikes.FormattingEnabled = true;
+            ChoiceOfBikes.Items.AddRange(new object[] { "Urban Arrow 4 pers. € 20", "Urban Arrow 6 pers. € 30", "Urban Arrow 4 pers. E-Bike € 40", "Urban Arrow 6 pers. E-Bike € 60" });
+            ChoiceOfBikes.Location = new Point(114, 59);
+            ChoiceOfBikes.Name = "ChoiceOfBikes";
+            ChoiceOfBikes.Size = new Size(252, 29);
+            ChoiceOfBikes.TabIndex = 14;
+            ChoiceOfBikes.Text = "Urban Arrow 4 pers. € 20";
+            // 
+            // ExtrasList
+            // 
+            ExtrasList.CheckOnClick = true;
+            ExtrasList.Font = new Font("Segoe UI", 12F);
+            ExtrasList.FormattingEnabled = true;
+            ExtrasList.Items.AddRange(new object[] { "Telefoonhouder € 5", "Helm € 10", "Regenhoes € 15", "Babystoel € 20" });
+            ExtrasList.Location = new Point(114, 146);
+            ExtrasList.Name = "ExtrasList";
+            ExtrasList.Size = new Size(190, 100);
+            ExtrasList.TabIndex = 16;
+            ExtrasList.ThreeDCheckBoxes = true;
+            // 
+            // ButtonCalculateCost
+            // 
+            ButtonCalculateCost.Enabled = false;
+            ButtonCalculateCost.Font = new Font("Segoe UI", 12F);
+            ButtonCalculateCost.Location = new Point(423, 336);
+            ButtonCalculateCost.Name = "ButtonCalculateCost";
+            ButtonCalculateCost.Size = new Size(190, 43);
+            ButtonCalculateCost.TabIndex = 18;
+            ButtonCalculateCost.Text = "Bereken totale kosten";
+            ButtonCalculateCost.UseVisualStyleBackColor = true;
+            ButtonCalculateCost.Click += ButtonCalculateCost_Click;
+            // 
+            // TotalCostLabel
+            // 
+            TotalCostLabel.AutoSize = true;
+            TotalCostLabel.Font = new Font("Segoe UI", 12F);
+            TotalCostLabel.Location = new Point(433, 403);
+            TotalCostLabel.Name = "TotalCostLabel";
+            TotalCostLabel.Size = new Size(100, 21);
+            TotalCostLabel.TabIndex = 17;
+            TotalCostLabel.Text = "Totale kosten";
+            // 
+            // ExtrasLabel
+            // 
+            ExtrasLabel.AutoSize = true;
+            ExtrasLabel.Font = new Font("Segoe UI", 12F);
+            ExtrasLabel.Location = new Point(114, 113);
+            ExtrasLabel.Name = "ExtrasLabel";
+            ExtrasLabel.Size = new Size(216, 21);
+            ExtrasLabel.TabIndex = 15;
+            ExtrasLabel.Text = "Extras toevoegen (kosten p/d)";
+            // 
+            // BikeLabel
+            // 
+            BikeLabel.AutoSize = true;
+            BikeLabel.Font = new Font("Segoe UI", 12F);
+            BikeLabel.Location = new Point(114, 26);
+            BikeLabel.Name = "BikeLabel";
+            BikeLabel.Size = new Size(226, 21);
+            BikeLabel.TabIndex = 13;
+            BikeLabel.Text = "Selecteer uw gewenste bakfiets";
+            // 
+            // FormAddRental
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CostsPerDayLabel);
+            Controls.Add(totalDaysLabel);
+            Controls.Add(ReturnDatePicker);
+            Controls.Add(label1);
+            Controls.Add(StartLabel);
+            Controls.Add(RentDateLlabel);
+            Controls.Add(StartDatePicker);
+            Controls.Add(ChoiceOfBikes);
+            Controls.Add(ExtrasList);
+            Controls.Add(ButtonCalculateCost);
+            Controls.Add(TotalCostLabel);
+            Controls.Add(ExtrasLabel);
+            Controls.Add(BikeLabel);
+            Name = "FormAddRental";
+            Text = "FormAddRental";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label CostsPerDayLabel;
+        private Label totalDaysLabel;
+        private DateTimePicker ReturnDatePicker;
+        private Label label1;
+        private Label StartLabel;
+        private Label RentDateLlabel;
+        private DateTimePicker StartDatePicker;
+        private ComboBox ChoiceOfBikes;
+        private CheckedListBox ExtrasList;
+        private Button ButtonCalculateCost;
+        private Label TotalCostLabel;
+        private Label ExtrasLabel;
+        private Label BikeLabel;
     }
 }
--- VanDerBrinckesBakfietsenVerdieping/FormAddRental.cs
+++ VanDerBrinckesBakfietsenVerdieping/FormAddRental.cs
@@ -16,5 +16,10 @@ namespace VanDerBrinckesBakfietsenVerdieping
         {
             InitializeComponent();
         }
+
+        private void ButtonCalculateCost_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
